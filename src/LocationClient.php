851ba021f1e97bd161cec83a0f870558c97d0bb9
Jomaanro <mbrouwer@jomaanro.nl>
2016-02-28 12:53:18 +0100

    Add location

---
+++ src/LocationClient.php
@@ -0,0 +1,37 @@
+<?php namespace DivideBV\Postnl;
+
+/**
+ * Client class for CIF's shipping status service.
+ */
+class LocationClient extends BaseClient
+{
+
+    /**
+     * The URL of the production WSDL.
+     */
+    const PRODUCTION_WSDL = 'https://service.postnl.com/CIF/LocationWebService/2_0/?wsdl';
+
+    /**
+     * The URL of the sandbox WSDL.
+     */
+    const SANDBOX_WSDL = 'https://testservice.postnl.com/CIF_SB/LocationWebService/2_0/?wsdl';
+
+    /**
+     * @var array $classes
+     *     The complex types used by this client.
+     */
+    protected $classes = [
+        'GetNearestLocation',
+        'GetLocationsInArea',
+    ];
+
+    public function GetNearestLocation(ComplexTypes\GetNearestLocationsRequest $getNearestLocations)
+    {
+        return $this->__soapCall('GetNearestLocations', [$getNearestLocations]);
+    }
+
+    public function GetLocationsInArea(ComplexTypes\GetLocationsInAreaRequest $getLocationsInArea)
+    {
+        return $this->__soapCall('GetLocationsInArea', [$getLocationsInArea]);
+    }
+}
