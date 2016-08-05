8fef49e6e4c2a3df93abab1f0aa28b6d421b5f99
Jeroen Smit <jsmit@050media.nl>
2016-08-05 09:00:30 +0200

    Added return type to BaseType::create

--- src/ComplexTypes/BaseType.php
+++ src/ComplexTypes/BaseType.php
@@ -7,6 +7,8 @@ abstract class BaseType
 
     /**
      * Create a class instance statically without calling the constructor.
+     *
+     * @return $this
      */
     public static function create()
     {
