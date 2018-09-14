7c4212fd395f6db5033f19773b910b3a8d25f0fd
Wijnand Ross <wijnand@samson-it.nl>
2018-09-14 15:51:31 +0200

    Use SOAP_SINGLE_ELEMENT_ARRAYS

--- src/BaseClient.php
+++ src/BaseClient.php
@@ -47,9 +47,10 @@ abstract class BaseClient extends SoapClient
             'trace' => true,
             'stream_context' => stream_context_create([
                 'http' => [
-                    'header' => "apikey: $apikey"
+                    'header' => "apikey: $apikey",
                 ],
             ]),
+            'features' => SOAP_SINGLE_ELEMENT_ARRAYS,
         ]);
     }
 
